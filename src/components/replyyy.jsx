cc359908c5824e20f76b6509771c1da296bc6d4e
holybee009 <abdulmuminoluwafemioshodi@gmail.com>
2024-02-11 10:53:07 +0000

    hope

--- src/components/replyyy.jsx
+++ src/components/replyyy.jsx
@@ -15,30 +15,32 @@ function Reply({
   newReply,
 }) {
   return (
-    <div className={classes.post}>
-      <div className={classes.votes}>
-        <img src={plus} alt="plus" onClick={() => addVote()} />
-        <div className={classes.voteNum}>{voteValue}</div>
-        <img src={minus} alt="minus" onClick={() => lessVote()} />
-      </div>
-      <div className={classes.bodyBox}>
-        <div className={classes.top}>
-          <div className={classes.leftSide}>
-            <img src={userImage} alt="image" className={classes.image} />
-            <h3 className={classes.user}>{userName}</h3>
-            <h3 className={classes.period}>{userPeriod}</h3>
+    <>
+      <div className={classes.post}>
+        <div className={classes.votes}>
+          <img src={plus} alt="plus" onClick={() => addVote()} />
+          <div className={classes.voteNum}>{voteValue}</div>
+          <img src={minus} alt="minus" onClick={() => lessVote()} />
+        </div>
+        <div className={classes.bodyBox}>
+          <div className={classes.top}>
+            <div className={classes.leftSide}>
+              <img src={userImage} alt="image" className={classes.image} />
+              <h3 className={classes.user}>{userName}</h3>
+              <h3 className={classes.period}>{userPeriod}</h3>
+            </div>
+            <div className={classes.rightSide} onClick={newReply}>
+              <img src={repl} alt="reply" className={classes.reply} />
+              <p className={classes.rep}>reply</p>
+            </div>
           </div>
-          <div className={classes.rightSide} onClick={newReply}>
-            <img src={repl} alt="reply" className={classes.reply} />
-            <p className={classes.rep}>reply</p>
+          <div className={classes.body}>
+            <span className={classes.tag}>@{userReplied}</span>
+            {userComment}
           </div>
         </div>
-        <div className={classes.body}>
-          <span className={classes.tag}>@{userReplied}</span>
-          {userComment}
-        </div>
       </div>
-    </div>
+    </>
   );
 }
 
